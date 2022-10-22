0ef61645f633c64658b1b4b5d3a3843d2f6f098e
richard-ioi <richard.fouquoire@edu.esiee.fr>
2022-10-22 17:17:44 +0200

    maj

--- PassportRefresh.py
+++ PassportRefresh.py
@@ -1,6 +1,7 @@
 # Import the required modules
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 
 def getAnswer():
@@ -12,6 +13,13 @@ def getAnswer():
         elif(answer=='N'):
             return False
 
+def connect():
+    driver.get('https://v70-auth.paris.fr/auth/realms/paris/protocol/openid-connect/auth?client_id=W78-PROD&response_type=code&redirect_uri=https%3A%2F%2Fteleservices.paris.fr%2Frdvtitres%2Fservlet%2Fplugins%2Foauth2%2Fcallback%3Fdata_client%3DauthData&scope=openid+profile+email+address+phone&state=238de4c1cb588&nonce=1be04a38f9f20')
+    driver.find_element(By.NAME,'username').send_keys(login)
+    driver.find_element(By.NAME,'password').send_keys(password)
+    driver.find_element(By.NAME,'Submit').click()
+    
+
 def check_through_mairies():
     i=28
     driver.get('https://teleservices.paris.fr/rdvtitres/jsp/site/Portal.jsp?page=appointment&view=getViewAppointmentCalendar&id_form='+str(i)+'&nbPlacesToTake=1')
@@ -22,48 +30,44 @@ def check_through_mairies():
             i+=1
         driver.get('https://teleservices.paris.fr/rdvtitres/jsp/site/Portal.jsp?page=appointment&view=getViewAppointmentCalendar&id_form='+str(i)+'&nbPlacesToTake=1')
 
-    driver.find_element_by_class_name("fc-event-container").click()
+    driver.find_element(By.CLASS_NAME,'fc-event-container').click()
     appointment_fill_infos()
 
 def check_exists_by_id(id):
     try:
-        driver.find_element_by_id(id)
+        driver.find_element(By.ID,id)
     except NoSuchElementException:
         return False
     return True
 
 def check_exists_by_class_name(class_name):
     try:
-        driver.find_element_by_class_name(class_name)
+        driver.find_element(By.CLASS_NAME,class_name)
     except NoSuchElementException:
         return False
     return True
 
 def appointment_fill_infos():
-    driver.find_element_by_name('lastname').send_keys(lastname)
-    driver.find_element_by_name('firstname').send_keys(firstname)
-    driver.find_element_by_name('email').send_keys(email)
-    driver.find_element_by_name('emailConfirm').send_keys(email)
 
     i=0
     while(check_exists_by_id('attribute'+str(i)) == False):
         i+=1
-    driver.find_element_by_name('attribute'+str(i)).send_keys(phone)
+    driver.find_element(By.NAME,'attribute'+str(i)).send_keys(phone)
     i+=1
     while(check_exists_by_id('attribute'+str(i)) == False):
         i+=1
-    driver.find_element_by_name('attribute'+str(i)).send_keys(postalCode)
+    driver.find_element(By.NAME,'attribute'+str(i)).send_keys(postalCode)
 
-    driver.find_element_by_name("save").click()
+    driver.find_element(By.NAME,"save").click()
+    
 
 
 # Main Function
 if __name__ == '__main__':
 
-    firstname='Jean'
-    lastname='DUPONT'
-    postalCode='75001'
-    email='mail.example@mail.fr'
+    login="login@mail.com"
+    password="PASSWORD"
+
     phone='0612345678'
     
     # print("=========")
@@ -110,14 +114,15 @@ if __name__ == '__main__':
 
     print("\n=========")
 
-    options = webdriver.ChromeOptions()
+    options = webdriver.EdgeOptions()
     options.add_argument("--start-maximized")
     options.add_argument('--log-level=3')
 
     # Provide the path of chromedriver present on your system.
-    driver = webdriver.Chrome(executable_path="Drivers/chromedriver.exe", options = options)
+    driver = webdriver.Edge(executable_path="Drivers/msedgedriver.exe", options = options)
     driver.set_window_size(1920,1080)
 
+    connect()
     check_through_mairies()
 
     # Quits the driver
